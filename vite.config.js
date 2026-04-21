f2e1ed32e2c8acd5c1e13145f409d7e443206272
yukijames25 <yukijames25@gmail.com>
2026-04-21 23:37:00 +0900

    修正

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // 例: 1000KB (1MB) に引き上げる
+  }
 })
